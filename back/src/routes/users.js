72a8266344c3c3cb1ba0a9104939131b940ecf3d
Manuel Pérez Jurado <perezjuradomanuel@gmail.com>
2023-08-10 13:43:17 +0200

    ruta de creción, edición y modificación de usuarios

---
+++ back/src/routes/users.js
@@ -0,0 +1,31 @@
+const { Router } = require("express");
+
+const userRouter = Router();
+
+const {
+    userByIdHandler,
+    createUserHandler,
+    getUsersHandler,
+    editUserHandler,
+    deleteUserHandler,
+    setAdminHandler,
+  } = require("../handlers/usersHandler");
+
+userRouter.get("/", checkAuth, checkAdminAuth([true]), getUsersHandler);
+userRouter.post("/", createUserHandler);
+userRouter.put(
+  "/admin/:id",
+  checkAuth,
+  checkAdminAuth([true]),
+  setAdminHandler
+);
+userRouter.delete(
+  "/delete/:id",
+  checkAuth,
+  checkAdminAuth([true]),
+  deleteUserHandler
+); // La ruta es exepcionalmente mas explicita para evitar accidentes
+userRouter.get("/:id", checkAuth, checkAdminAuth([true]), userByIdHandler);
+userRouter.put("/:id", checkAuth, checkAdminAuth([true]), editUserHandler);
+
+module.exports = userRouter;
